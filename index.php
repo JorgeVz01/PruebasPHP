fba30b9ebac06c3983605ec2f3c4ea3c3819e68c
JorgeVz01 <jorgebarvaz@gmail.com>
2023-10-23 13:16:52 +0200

    index y  pruebas

---
+++ index.php
@@ -0,0 +1,30 @@
+<h1>HTML</h1>
+
+<?php
+    echo "<h1>codigo php </h1>";
+    print("hola");
+
+    $nombre = "Jorge";
+    $texto = "variable texto con otra variable $nombre";
+    $num = 888;
+
+    #puntos para concatenar
+
+    echo "<h2>" .$texto. " numero: ". $num. "</h2>";
+   
+    $numero = 0;
+
+while ($numero <= 100) {
+    if ($numero % 5 == 0) {
+        echo $numero . " ";
+    }
+    $numero++;
+}
+echo '<br><hr><br>';
+
+for ($numero = 320; $numero >= 160; $numero -= 20) {
+    echo $numero . " ";
+}
+?>
+<div></div>
+<br>
