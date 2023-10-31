9a07fa1c4b1dceb657afcdc58b4114feb94c8490
JorgeVz01 <jorgebarvaz@gmail.com>
2023-10-31 12:06:09 +0100

    aab

---
+++ ACT2/index2.php
@@ -0,0 +1,25 @@
+<h1>HTML</h1>
+
+<?php
+    echo "<h1>codigo php </h1>";
+    print("hola");
+
+    $nombre = "Jorge";
+    $texto = "variable texto con otra variable $nombre";
+    $num = 888;
+
+    #puntos para concatenar
+
+    echo "<h2>" .$texto. " numero: ". $num. "</h2>";
+   
+    $numero = 0;
+
+while ($numero <= 100) {
+    if ($numero % 5 == 0) {
+        echo $numero . " ";
+    }
+    $numero++;
+}
+echo '<br><hr><br>';
+
+
